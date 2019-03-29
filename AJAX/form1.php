277d577d5621aeca4a332d5fb56199490ccb9efd
26583 <43673592+26583@users.noreply.github.com>
2019-03-29 14:52:10 +0100

    Add files via upload

---
+++ AJAX/form1.php
@@ -0,0 +1,14 @@
+<?php
+// Naam: Pelle Meijer, Klas: GD1B;
+ $firstName = $_GET['firstName'];
+ $age = $_GET['age'];
+ $lastName = $_GET['lastName'];
+ $email = $_GET['email'];
+ echo "<h2>Response Demo Form</h2><h3> ";
+ echo "You submitted the following information:<br><ul>";
+ echo "<li>Name: $firstName</li>";
+ echo "<li>Lastname: $lastName</li>";
+ echo "<li>age: $age</li>";
+ echo "<li>email: $email</li>";
+ echo "</ul></h3>";
+ ?>
